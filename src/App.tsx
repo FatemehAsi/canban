14ba8b2f9d6418b666cefa4e12cf0e70320def6e
dev-asi01 <fatemeh.asi.2000@gmail.com>
2025-09-17 10:06:50 -0700

    [ADD] content and styles to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,76 @@
-import {type ReactNode} from 'react';
+// import {type ReactNode} from 'react';
 
-import './App.css'
+import styles from "./App.module.css";
+import clsx from "clsx";
 
-function App() {
+export default function App(){
+  return(
+    <div className={styles.app}>
+      <header>Header</header>
+      <main>
+        <div className={styles.header}>
+          <h1 className={styles.title}>Boards</h1>
+          <button>Create</button>
+        </div>
 
-  return (
-    <>
-      <div>Hello, friend</div>
-    </>
+        <ul className={styles.boards}>
+          <li>
+          <div className={clsx(styles.board, "blue")}>
+            <div className={styles.cover}></div>
+            <div className={styles.content}>
+            <div className={styles.header}>
+              <div className={styles.title}>Board1</div>
+              <a href='/board'>View</a>
+            </div>
+            <p className={styles.description}>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Doloribus ducimus libero accusantium nemo laudantium, quasi obcaecati provident nam vero cumque, sequi rem, id similique. Assumenda maiores nisi ipsam iure illo.
+            </p>
+          </div>
+          </div>   
+          </li>
+
+          <li>
+            <div className={clsx(styles.board, "gray")}>
+                   <div className={styles.cover}></div>
+          <div className={styles.content}>
+            <div className={styles.header}>
+              <div className={styles.title}>Board1</div>
+              <a href='/board'>View</a>
+            </div>
+            <p className={styles.description}>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Doloribus ducimus libero accusantium nemo laudantium, quasi obcaecati provident nam vero cumque, sequi rem, id similique. Assumenda maiores nisi ipsam iure illo.
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quas sit animi ipsa, veritatis unde suscipit dolores fugit sed eligendi voluptate totam. Aliquid optio fuga enim necessitatibus, rerum aperiam delectus inventore!
+            </p>
+          </div>
+          
+            </div>
+          </li>
+
+          <li>
+            <div className={clsx(styles.board, "yellow")}>
+              <div className={styles.cover}></div>
+              <div className={styles.content}>
+                <div className={styles.header}>
+                <div className={styles.title}>Board1</div>
+                <a href='/board'>View</a>
+              </div>
+              <p className={styles.description}>
+                 Lorem ipsum dolor sit, amet consectetur adipisicing elit. Doloribus ducimus libero accusantium nemo laudantium, quasi obcaecati provident nam vero cumque, sequi rem, id similique. Assumenda maiores nisi ipsam iure illo.
+                 Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quas sit animi ipsa, veritatis unde suscipit dolores fugit sed eligendi voluptate totam. Aliquid optio fuga enim necessitatibus, rerum aperiam delectus inventore!
+              </p>
+            </div>
+            
+            </div>
+          </li>
+
+        </ul>
+
+      </main>
+
+      <footer>
+        
+      </footer>
+    </div>
   )
-}
 
-export default App
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+
+import App from './App.tsx';
+
+import './index.css';
+import './styles/colors.css';
+import './styles/shadows.css';
+import './styles/shapes.css';
+import './styles/typography.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
