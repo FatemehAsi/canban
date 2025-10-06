3d8a87c64fbf08a80f5b1143df5fcafb9cb5e2da
dev-asi01 <fatemeh.asi.2000@gmail.com>
2025-10-06 14:35:30 -0700

    [ADD] Header component

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,9 @@
+import { ReactNode } from "react";
+
+import styles from "./Header.module.css";
+
+export default function Header(){
+    return(
+        <div className={styles.header}></div>
+    )
+}
