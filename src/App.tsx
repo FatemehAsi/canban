da8523af2894054903ecbe004dc0c7dde5bf66b0
dev-asi01 <fatemeh.asi.2000@gmail.com>
2025-10-06 13:20:26 -0700

    [ADD] Board Page

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@
 import { Routes, Route } from "react-router";
 
 import HomePage from "./pages/HomePage/HomePage";
+import BoardPage from "./pages/HomePage/BoardPage/BoardPage";
 
 export default function App(){
   return(
@@ -9,6 +10,7 @@ export default function App(){
       <Routes>
         <Route index element={<HomePage />}/>
         {/*index همان path="/" است بهتر است از index استفاده کنیم  */}
+        <Route path="board" element={<BoardPage />} />
       </Routes>
     
    
---
+++ src/pages/HomePage/BoardPage/BoardPage.tsx
@@ -0,0 +1,10 @@
+import { ReactNode } from "react";
+
+import styles from "./BoardPage.module.css";
+
+export default function BoardPage(){
+    return(
+        <div className={styles["board-page"]}>Board Page</div>
+
+    );
+}
