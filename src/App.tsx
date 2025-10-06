b3930b4d7b6f6cf09a54398af7d50eb050d27670
dev-asi01 <fatemeh.asi.2000@gmail.com>
2025-10-06 13:12:15 -0700

    [ADD] react router [ADD] Home Page

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,17 @@
 // import {type ReactNode} from 'react';
+import { Routes, Route } from "react-router";
 
-import styles from "./App.module.css";
-
-import BoardCard from "./components/BoardCard/BoardCard.tsx";
-import Button from "./components/Button/Button.tsx";
+import HomePage from "./pages/HomePage/HomePage";
 
 export default function App(){
   return(
-    <div className={styles.app}>
-      <header>Header</header>
-      <main>
-        <div className={styles.header}>
-          <h1 className={styles.title}>Boards</h1>
-          <Button color="primary">Create</Button>
-        </div>
-
-        <ul className={styles.boards}>
-            <li>
-                <BoardCard id={1} title="Spring Tasks" description="lorem spammmmmmmmmmmmmmmmmmmmm jjjjjjjjfklsfnhhnkiuasdi" color="blue" />
-            </li>
-
-            <li>
-                <BoardCard id={2} title="Content Calendar" description="adwd skfjolijfr sejhinwieh  wirhwiu uuhi n wjrhdwiuh hdffiush iejopqwje " color="gray" />
-            </li>
-
-            <li>
-                <BoardCard id={3} title="Personal Goals" description="ewrfwsfgb dfgdrhgr regr hjgghrf drfed sfer dr berfe" color="yellow" />
-            </li>
-
-        </ul>
-
-      </main>
-
-      <footer>
-        
-      </footer>
-    </div>
+    
+      <Routes>
+        <Route index element={<HomePage />}/>
+        {/*index همان path="/" است بهتر است از index استفاده کنیم  */}
+      </Routes>
+    
+   
   )
 
 }
---
+++ src/pages/HomePage/HomePage.tsx
@@ -0,0 +1,38 @@
+import type { ReactNode } from "react";
+
+import styles from "./HomePage.module.css";
+
+import BoardCard from  "../../components/BoardCard/BoardCard.tsx";
+import Button from "../../components/Button/Button.tsx";
+
+export default function HomePage(): ReactNode{
+    return <div className={styles["home-page"]}>
+          <header>Header</header>
+      <main>
+        <div className={styles.header}>
+          <h1 className={styles.title}>Boards</h1>
+          <Button color="primary">Create</Button>
+        </div>
+
+        <ul className={styles.boards}>
+            <li>
+                <BoardCard id={1} title="Spring Tasks" description="lorem spammmmmmmmmmmmmmmmmmmmm jjjjjjjjfklsfnhhnkiuasdi" color="blue" />
+            </li>
+
+            <li>
+                <BoardCard id={2} title="Content Calendar" description="adwd skfjolijfr sejhinwieh  wirhwiu uuhi n wjrhdwiuh hdffiush iejopqwje " color="gray" />
+            </li>
+
+            <li>
+                <BoardCard id={3} title="Personal Goals" description="ewrfwsfgb dfgdrhgr regr hjgghrf drfed sfer dr berfe" color="yellow" />
+            </li>
+
+        </ul>
+
+      </main>
+
+      <footer>
+        
+      </footer>
+    </div>
+}
