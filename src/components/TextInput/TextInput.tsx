484b16e7e935e074bdf03a5b6244a2540fe57678
dev-asi01 <fatemeh.asi.2000@gmail.com>
2025-11-09 10:51:38 -0800

    [ADD] controlled input example

--- src/components/TextInput/TextInput.tsx
+++ src/components/TextInput/TextInput.tsx
@@ -1,4 +1,4 @@
-import { type ReactNode, type ComponentProps, useId } from "react";
+import { type ReactNode, type ComponentProps, useId, type ChangeEvent, useState } from "react";
 
 import styles from "../TextInput/TextInput.module.css";
 import clsx from "clsx";
@@ -10,11 +10,29 @@ type Props = ComponentProps<'input'> & {
 
 export default function TextInput({className, lable, error, ...otherProps}: Props): ReactNode{
     const id = useId();
+
+    const [value, setValue] = useState<string>('');
+
+    const handleInputChange = (e: ChangeEvent<HTMLInputElement>): void => {
+
+        const n = Number.parseInt(e.target.value.replaceAll(/\D/g, ""));
+
+        if(Number.isNaN(n)){
+            return;
+        }
+
+        // const value = e.target.value;
+        // console.log((+value).toLocaleString());
+        setValue(n.toLocaleString());
+        
+    }
+
+
     return(
         <div className={clsx(styles["text-input"], !!error && styles.error, className)}>
 
             <label htmlFor={id}>{lable}</label>
-            <input id={id} {...otherProps} />
+            <input id={id} {...otherProps} value={value} onChange={handleInputChange}/>
             <span className={styles.error}>{error || "\u00A0"}</span>
 
         </div>
